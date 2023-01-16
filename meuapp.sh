eac8865edf79a4475941317c054ce1df7005dc24
Manoloko15 <122757621+Manoloko15@users.noreply.github.com>
2023-01-15 21:05:39 -0300

    Add files via upload

---
+++ meuapp.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Olá, este programa irá printar os processo com tal nome, e criar um executável para vc apos selecionar um pid
+# Feel free to change it :)
+
+set -e
+echo "Fala ${USER}!"
+echo Por qual processo posso buscar?
+echo [ Help ] este input aceita parametros do pgrep. Mais informações: pgrep --help
+read -p "p: " nomedeprocesso
+piddoprocesso=$(pgrep $nomedeprocesso)
+echo $piddoprocesso
+for npid in $piddoprocesso
+do
+    printf '\033[?7l'
+    echo ""
+    echo pid: $npid
+    printf "%-50s\n" $(ps -o cmd fp $npid) | awk '{ if ( NR > 1  ) { print } }'
+done
+echo ""
+echo ""
+echo Escreva o pid que lhe atrai:
+read -p 'p: ' piddebusca
+final=$(ps -o cmd fp $piddebusca | awk '{ if ( NR > 1  ) { print } }')
+echo c: $final
+echo '#!/bin/sh' > comando.sh
+echo konsole -e '"'$final'"' >> comando.sh
+chmod +x comando.sh
+echo '[comando.sh] criado para execução do comando selecionado!'
+exit 1
